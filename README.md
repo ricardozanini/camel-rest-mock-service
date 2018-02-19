d2fb544df7ccc8aad387a2a470446799819dc194
Ricardo Zanini <ricardozanini@gmail.com>
2018-02-19 11:06:27 -0300

    initialize multiple servers

--- README.md
+++ README.md
@@ -46,3 +46,43 @@ firewall-cmd --reload
 
 ## Tomcat Tuning
 
+## Apache Tunning
+
+Refs:
+
+1. [Sizing MaxClients](https://httpd.apache.org/docs/trunk/misc/perf-scaling.html#sizing-maxClients)
+
+### MPM Event Bug
+
+One of the justifications to use worker is a bug on `event`, as stated on [this KCS article from Red Hat](https://access.redhat.com/solutions/3035211): `AH00485: scoreboard is full, not at MaxRequestWorkers`. 
+
+The jbcs-httpd version used on this lab is `2.2.23` (waiting for the fix):
+
+```log
+Server version: Apache/2.4.23 (Red Hat)
+Server built:   Nov  6 2017 12:21:08
+Server's Module Magic Number: 20120211:61
+Server loaded:  APR 1.5.2, APR-UTIL 1.5.4
+Compiled using: APR 1.5.2, APR-UTIL 1.5.4
+Architecture:   64-bit
+Server MPM:     event
+  threaded:     yes (fixed thread count)
+    forked:     yes (variable process count)
+Server compiled with....
+ -D APR_HAS_SENDFILE
+ -D APR_HAS_MMAP
+ -D APR_HAVE_IPV6 (IPv4-mapped addresses enabled)
+ -D APR_USE_SYSVSEM_SERIALIZE
+ -D APR_USE_PTHREAD_SERIALIZE
+ -D SINGLE_LISTEN_UNSERIALIZED_ACCEPT
+ -D APR_HAS_OTHER_CHILD
+ -D AP_HAVE_RELIABLE_PIPED_LOGS
+ -D DYNAMIC_MODULE_LIMIT=256
+ -D HTTPD_ROOT="/opt/rh/jbcs-httpd24/root/etc/httpd"
+ -D SUEXEC_BIN="/opt/rh/jbcs-httpd24/root/usr/sbin/suexec"
+ -D DEFAULT_PIDLOG="/run/httpd/httpd.pid"
+ -D DEFAULT_SCOREBOARD="logs/apache_runtime_status"
+ -D DEFAULT_ERRORLOG="logs/error_log"
+ -D AP_TYPES_CONFIG_FILE="conf/mime.types"
+ -D SERVER_CONFIG_FILE="conf/httpd.conf"
+```
--- bin/start.sh
+++ bin/start.sh
@@ -2,12 +2,45 @@
 
 DIRNAME=`dirname $0`
 PROGNAME=`basename $0`
+DEFAULT_SERVERS=1
+DEFAULT_MEM=512
+DEFAULT_META=$(( $DEFAULT_MEM / 8 ))
+DEFAULT_S_PORT=8081
+DEFAULT_M_PORT=8083
+DEFAULT_JMX_PORT=12348
+
+SERVERS=$DEFAULT_SERVERS
+MEM=$DEFAULT_MEM
+META=$DEFAULT_META
 
 die() {
     warn "$*"
     exit 1
 }
 
+calcMem() {
+    if [ "x$1" = "x" ] || [ $1 = $DEFAULT_MEM ]; then
+        MEM=$DEFAULT_MEM
+        META=$DEFAULT_META
+    else
+        MEM=$1
+        META=$(( $MEM / 8 ))
+    fi
+
+    echo "============================================="
+    echo "JVM Max size is $MEM"
+    echo "Max Metaspace sice is $META"
+    echo "============================================="
+}
+
+setServersNum() {
+    if [ "x$1" = "x" ]; then
+        SERVERS=$DEFAULT_SERVERS
+    else
+        SERVERS=$1
+    fi
+    echo "Servers instances set to $SERVERS"
+}
 
 locateJava() {
     # Setup the Java Virtual Machine
@@ -48,28 +81,50 @@ startService() {
     cd $SERVICE_HOME
     SERVICE_JAR=`ls *.jar`
     # external properties https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-external-config.html
-    echo "Starting service at $SERVICE_HOME"
+    echo "Starting services at $SERVICE_HOME"
     
-    # https://stackoverflow.com/questions/29412072/how-to-access-spring-boot-jmx-remotely
-    JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=12348 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.rmi.port=12349"
-    GC_OPTS="-XX:+PrintGCApplicationStoppedTime -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -Xloggc:$SERVICE_HOME/log/gc.log -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$SERVICE_HOME/log/"
-    JVM_OPTS="-server -XX:+DoEscapeAnalysis -XX:+UseParallelGC -XX:+UseParallelOldGC -Xmx1024M -Xms1024M -XX:+AggressiveOpts -XX:+UseCompressedOops -XX:+UseCompressedClassPointers -XX:CompressedClassSpaceSize=512M -XX:MetaspaceSize=128M -XX:MaxMetaspaceSize=128M"
-    rm -rf "$SERVICE_HOME/log/*"
-    touch "$SERVICE_HOME/log/gc.log"
+    # clean up old logs
+    rm -rf "$SERVICE_HOME/log"
+    mkdir "$SERVICE_HOME/log"
 
-    # go!
-    $JAVA -DLOG_DIR=$SERVICE_HOME $JVM_OPTS  $GC_OPTS $JMX_OPTS -jar $SERVICE_JAR --logging.config=file:$START_HOME/async-logback.xml --spring.config.location=file:$START_HOME/application.yml
+    # vars
+    JMX_PORT=$DEFAULT_JMX_PORT
+    SERVER_PORT=$DEFAULT_S_PORT
+    MGMN_PORT=$DEFAULT_M_PORT
+
+    for ((S=0; S<$SERVERS; S++))
+    do
+        # https://stackoverflow.com/questions/29412072/how-to-access-spring-boot-jmx-remotely
+        JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=${JMX_PORT} -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+        GC_OPTS="-XX:+PrintGCApplicationStoppedTime -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -Xloggc:$SERVICE_HOME/log/gc.${S}.log -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$SERVICE_HOME/log/"
+        JVM_OPTS="-server -Xmx${MEM}M -Xms${MEM}M -XX:+AggressiveOpts -XX:MetaspaceSize=${META}M -XX:MaxMetaspaceSize=${META}M -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark"
+        
+        touch "$SERVICE_HOME/log/gc.$S.log"
+
+        # go!
+        $JAVA -DLOG_DIR=$SERVICE_HOME -Dserver.port=$SERVER_PORT -Dmanagement.port=$MGMN_PORT $JVM_OPTS  $GC_OPTS $JMX_OPTS -jar $SERVICE_JAR --logging.config=file:$START_HOME/async-logback.xml --spring.config.location=file:$START_HOME/application.yml &
+
+        echo ">> Started server on port $SERVER_PORT, management port on $MGMN_PORT and JMX remote port on $JMX_PORT"
+
+        JMX_PORT=$((JMX_PORT+1))
+        SERVER_PORT=$((SERVER_PORT+100))
+        MGMN_PORT=$((MGMN_PORT+100))
+    done
+    
 }
 
 init() {
     # locate the service home
     locateHome
     locateStartHome
+    calcMem $1
+    setServersNum $2
     # locate java
     locateJava
     # start the service
     startService
 }
 
-init
+# $1 = memory, $2 server instances
+init $1 $2
 
