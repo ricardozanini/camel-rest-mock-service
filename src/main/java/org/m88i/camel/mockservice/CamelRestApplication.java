773179390c329b1e7942156e22733a04021d2f03
Ricardo Zanini <ricardozanini@gmail.com>
2018-08-29 15:13:03 -0300

    Tomcat customizer

--- src/main/java/org/m88i/camel/mockservice/CamelRestApplication.java
+++ src/main/java/org/m88i/camel/mockservice/CamelRestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -14,39 +15,48 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "org.m88i.camel.mockservice")
 public class CamelRestApplication {
-    
+
     private static final String PROTOCOL = "AJP/1.3";
     private static final String MAX_CONN_PROP = "maxConnections";
     private static final String ACCEPT_COUNT_PROP = "acceptCount";
 
     @Value("${server.tomcat.ajp-port}") // Defined on application.properties
     private Integer ajpPort;
-    
-    @Value("${server.tomcat.max-connections}") 
+
+    @Value("${server.tomcat.max-connections}")
     private Integer maxConnections;
-    
-    @Value("${server.tomcat.accept-count}") 
+
+    @Value("${server.tomcat.accept-count}")
     private Integer acceptCount;
 
     public static void main(String[] args) {
         SpringApplication.run(CamelRestApplication.class, args);
     }
-    
+
+    @Bean
+    public TomcatConnectorCustomizer customizer(TomcatEmbeddedServletContainerFactory tomcat) {
+        return new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                if (maxConnections != null) {
+                    connector.setProperty(MAX_CONN_PROP, maxConnections.toString());
+                }
+                if (acceptCount != null) {
+                    connector.setProperty(ACCEPT_COUNT_PROP, acceptCount.toString());
+                }
+            }
+        };
+    }
+
     @Bean
     public EmbeddedServletContainerFactory servletContainer() {
         TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addConnectorCustomizers(this.customizer(tomcat));
         Connector ajpConnector = new Connector(PROTOCOL);
-        ajpConnector.setProtocol(PROTOCOL);
         ajpConnector.setPort(ajpPort);
-        if(maxConnections != null) {
-            ajpConnector.setProperty(MAX_CONN_PROP, maxConnections.toString());
-        }
-        if(acceptCount != null) {
-            ajpConnector.setProperty(ACCEPT_COUNT_PROP, acceptCount.toString());
-        }
         tomcat.addAdditionalTomcatConnectors(ajpConnector);
         return tomcat;
-    }   
+    }
 
     @Bean
     ServletRegistrationBean servletRegistrationBean() {
